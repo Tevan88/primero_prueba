0e3cc2fb481b7b7f2d62302c1c3f46531bb23b93
Tevan88 <tevanrivarola@gmail.com>
2025-03-29 13:29:49 -0300

    se modifico primero prueba

--- primero prueba.py
+++ primero prueba.py
@@ -1,5 +1,5 @@
-nombre = "Esteban"
-apellido = "Rivarola"
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
 materia = "Programacion 1"
 
 print(f"Hola {nombre}, tu apellido es {apellido} y estas en la materia {materia}")
