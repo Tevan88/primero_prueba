2bb86bef508164d4b2b1edae6b8660c85fdacfb2
Tevan88 <tevanrivarola@gmail.com>
2025-03-29 13:32:06 -0300

    se agregaron comentarios a primero prueba

--- primero prueba.py
+++ primero prueba.py
@@ -1,5 +1,8 @@
+#Se define variable nombre
 nombre = "Esteban"
+#Se define variable apellido
 apellido = "Rivarola"
+#Se define variable materia
 materia = "Programacion 1"
-
+#Se imprime mensaje por consola
 print(f"Hola {nombre}, tu apellido es {apellido} y estas en la materia {materia}")
