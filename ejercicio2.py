185d0c070daa1ac68efd57b7b74119c6b376bcfc
Tevan88 <tevanrivarola@gmail.com>
2025-03-29 13:19:24 -0300

    se creo ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+materia = "Programacion 1"
+
+print(f"Hola {nombre}, tu apellido es {apellido} y estas en la materia {materia}")
